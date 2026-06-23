df58891b3e56e52943cc4a4dd29744af2e1f9a27
GodOnAbreak <andresebastianlalmuhairi@gmail.com>
2026-06-23 16:27:07 +0800

    Add files via upload

---
+++ ArtistSiteContent_FINAL/pale.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A Pale White Dot</title>
+</head>
+<body>
+
+<video controls autoplay>
+    <source src="media/pale.mp4" type="video/mp4">
+</video>
+
+</body>
+</html>
